4597c20e66c575444d0ebfea12defee1e224d345
Yadunand Prem <yadunut@gmail.com>
2023-01-02 15:48:55 +0800

    feat(gateway): merge config into main

--- backend/api-gateway/cmd/server/main.go
+++ backend/api-gateway/cmd/server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"reflect"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/yadunut/CVWO/backend/api-gateway/internal/graph"
 	"github.com/yadunut/CVWO/backend/api-gateway/internal/proto"
 	"go.uber.org/zap"
@@ -12,29 +17,25 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func initResolver(config Config) (*graph.Resolver, error) {
-
-	cc, err := grpc.Dial(config.AuthServiceUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, err
-	}
-
-	return &graph.Resolver{AuthClient: proto.NewAuthServiceClient(cc)}, nil
+type Config struct {
+	Port           string `default:"8080"`
+	AuthServiceUrl string `split_words:"true"`
 }
+
 
 func main() {
 	logger := zap.Must(zap.NewDevelopment())
 	defer logger.Sync()
 	log := logger.Sugar()
 
-	config, err := LoadConfig()
+	config, err := loadConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Debugf("%s", config)
 
-	LoadConfig()
+	loadConfig()
 
 	resolver, err := initResolver(config)
 	if err != nil {
@@ -48,4 +49,39 @@ func main() {
 
 	log.Infof("connect to http://localhost:%s/ for GraphQL playground", config.Port)
 	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
+}
+
+func initResolver(config Config) (*graph.Resolver, error) {
+	cc, err := grpc.Dial(config.AuthServiceUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	return &graph.Resolver{AuthClient: proto.NewAuthServiceClient(cc)}, nil
+}
+
+
+func loadConfig() (c Config, err error) {
+	// only call load if .env exists
+	if _, err = os.Stat(".env"); !os.IsNotExist(err) {
+		err = godotenv.Load()
+		if err != nil {
+			return
+		}
+	}
+
+	err = envconfig.Process("CVWO", &c)
+	if err != nil {
+		return
+	}
+
+	cRef := reflect.ValueOf(&c).Elem()
+	for i := 0; i < cRef.NumField(); i++ {
+		field := cRef.Field(i)
+		if field.IsZero() {
+			err = fmt.Errorf("%s cannot be empty", cRef.Type().Field(i).Name)
+			return
+		}
+	}
+	return
 }
